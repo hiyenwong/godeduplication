96cfb22c54dadf7e410eadb240d5d8098f7eb185
Hai Yan Wang <robinwong51@qq.com>
2015-03-21 22:11:07 +0800

    dedup string

---
+++ godeduplication.go
@@ -0,0 +1,14 @@
+package godeduplication
+
+func Dedup_String(str *[]String) {
+	found := make(map[string]bool)
+	timber := 0
+	for id, val := range str {
+		if !found[val] {
+			found[val] = true
+			(*str)[timber] = (*str)[id]
+			timber++
+		}
+	}
+	*str = (*str)[:timber]
+}
