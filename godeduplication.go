b36637ae68a9abdfdcf5e2f0c0edce63e5a22304
hyanwang <robinwong51@gmail.com>
2015-04-06 15:55:57 +0800

    change go

--- godeduplication.go
+++ godeduplication.go
@@ -1,9 +1,9 @@
 package godeduplication
 
-func Dedup_String(str *[]String) {
+func Dedup_String(str *[]string) {
 	found := make(map[string]bool)
 	timber := 0
-	for id, val := range str {
+	for id, val := range *str {
 		if !found[val] {
 			found[val] = true
 			(*str)[timber] = (*str)[id]
